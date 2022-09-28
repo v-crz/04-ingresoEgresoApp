6ce805d625a2929db5b8a369357c2298cbf86539
viry <vbatten10@hotmail.com>
2022-09-27 20:10:00 -0500

    Creando nuevo usuario

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -10,7 +12,11 @@ export class RegisterComponent implements OnInit {
 
   registroForm!: FormGroup;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
     this.registroForm = this.fb.group({
@@ -21,6 +27,13 @@ export class RegisterComponent implements OnInit {
   }
 
   crearUsuario() {
-
+    if( this.registroForm.invalid ){ return; }
+    const { nombre, correo, password } = this.registroForm.value;
+    this.authService.crearUsuario(nombre, correo, password)
+      .then( credenciales => {
+        console.log(credenciales);
+        this.router.navigate(['/']);
+      })
+      .catch( err => console.error(err));
   }
 }
