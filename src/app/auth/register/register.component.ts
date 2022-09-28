a9adb6048b76e5d581546b0ef8ce0b7614e78ac0
viry <vbatten10@hotmail.com>
2022-09-28 12:29:33 -0500

    Implementando SweetAlert2

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-register',
@@ -28,12 +29,29 @@ export class RegisterComponent implements OnInit {
 
   crearUsuario() {
     if( this.registroForm.invalid ){ return; }
+
+    Swal.fire({
+      title: 'Espere por favor',
+      didOpen: () => {
+        Swal.showLoading()
+      }
+    });
+
     const { nombre, correo, password } = this.registroForm.value;
     this.authService.crearUsuario(nombre, correo, password)
       .then( credenciales => {
         console.log(credenciales);
+        
+        Swal.close();
+
         this.router.navigate(['/']);
       })
-      .catch( err => console.error(err));
+      .catch( err => {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: err.message
+        })
+      });
   }
 }
