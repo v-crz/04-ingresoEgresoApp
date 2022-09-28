6a3efc5dad2785c5b874ca6bf52895e5cd2e956f
viry <vbatten10@hotmail.com>
2022-09-28 13:09:21 -0500

    Obtener información de usuario activo en Firebase

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -8,6 +8,12 @@ export class AuthService {
 
   constructor(public auth: AngularFireAuth) { }
 
+  initAuthListener(){
+    this.auth.authState.subscribe( fuser => {
+      console.log(fuser);
+    });
+  }
+
   crearUsuario( nombre: string, email: string, password: string ){
     return this.auth.createUserWithEmailAndPassword(email, password);
   }
