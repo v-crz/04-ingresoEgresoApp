d4198a76cb84db886cb48c340593ca8ec07b5ccd
viry <vbatten10@hotmail.com>
2022-09-29 13:07:21 -0500

    Auth Actions y Reducer

---
+++ src/app/auth/auth.actions.ts
@@ -0,0 +1,12 @@
+import { createAction, props } from '@ngrx/store';
+import { Usuario } from '../models/usuario.model';
+
+export const setUser = createAction(
+    '[Auth Component] setUser',
+    props<{ user: Usuario }>()
+);
+
+export const unSetUser = createAction(
+    '[Auth Component] unSetUser'
+);
+
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Store } from '@ngrx/store';
 import { map } from 'rxjs/operators';
+import { AppState } from '../app.reducer';
+import * as authActions from '../auth/auth.actions';
 import { Usuario } from '../models/usuario.model';
 
 @Injectable({
@@ -10,11 +13,20 @@ import { Usuario } from '../models/usuario.model';
 export class AuthService {
 
   constructor(public auth: AngularFireAuth,
-              private firestore: AngularFirestore) { }
+              private firestore: AngularFirestore,
+              private store: Store<AppState>) { }
 
   initAuthListener(){
     this.auth.authState.subscribe( fuser => {
-      console.log(fuser);
+      if(fuser){
+        this.firestore.doc(`${fuser.uid}/usuario`).valueChanges()
+          .subscribe(firestoreUser => {
+            const tempUser = new Usuario('abc','borrarme','abcjsj');
+            this.store.dispatch(authActions.setUser({user: tempUser}));
+          });
+      } else {
+
+      }
     });
   }
 
